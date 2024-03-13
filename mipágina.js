84a1755d905a3b393952efe148a2eb3afdf2d949
Pinedo97 <andrespinedo813@gmail.com>
2024-03-12 21:33:52 -0500

    Initial commit

---
+++ mipágina.js
@@ -0,0 +1,14 @@
+//alert("Hola este es mi Javascript");
+
+// let nombre ="Andres";
+// console.log(nombre)
+
+//let contenidoTitulo="Más información";
+let titulo=document.querySelector(".Contacto")
+//titulo.innerHTML = contenidoTitulo;
+
+//console.log(contenidoTitulo)
+
+//CONDICIONALES
+let textotitulo = titulo.innerText
+console.log(textotitulo);
